54f13a91bbfe8a05545255f41e993b5da9069ece
Alldbg <b.afellah.2018@gmail.com>
2022-05-19 04:18:16 +0000

    Add monitoring with Sentry.io

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 https://blog.openreplay.com/authentication-in-react-18-using-firebase-v9
 
 https://console.firebase.google.com/project/fir-v9-auth-centrale/authentication/providers
+
+https://blog.openreplay.com/front-end-monitoring-with-sentry-io-and-react
--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+
+Sentry.init({
+  dsn: "https://c4a9e4542c794d2c882d622f1b3de004@o1253128.ingest.sentry.io/6419840",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <button onClick={methodDoesNotExist}>Test Sentry Bad Button</button>;
     </BrowserRouter>
   </React.StrictMode>
 );
